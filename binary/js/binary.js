3bd3c94dfe1d452d71a2486e7c5d846faa4e8baf
hippy <hippy2981@gmail.com>
2020-12-15 20:09:09 -0800

    Add binary

---
+++ binary/js/binary.js
@@ -0,0 +1,69 @@
+//{ stubs
+
+if (typeof console === "undefined") {
+    console = {
+        log: function () { },
+        info: function () { },
+        warn: function () { },
+        error: function () { }
+    };
+}
+
+//}
+
+//{ Random
+
+function Random() {
+}
+Random.prototype.Next = function (limit) {
+    return Math.floor((Math.random() * limit));
+}
+
+//}
+
+//{ jquery
+
+$.fn.addClassTemporarily = function (className, time) {
+    var $this = this.addClass(className);
+    setTimeout(function () {
+        $this.removeClass(className);
+    }, time)
+    return this;
+};
+
+//}
+
+//{ GameConsole
+
+var items = [ "0", "1" ]
+var rng = new Random();
+
+function GameConsole() {
+}
+
+GameConsole.prototype.Roll = function () {
+    var number = items[rng.Next(items.length)];
+    var $item = $('#grid span');
+    $item.text(number)
+        .addClassTemporarily("wobble", 1000)
+        .stop(true).hide().stop(true).fadeIn();
+}
+
+//{ init
+
+function ehandler(event) {
+    if (event.keyCode == 13 || event.keyCode == 32 || event.type == 'tap') {
+        console.log('Enter/Space/Tap was pressed');
+        gameconsole.Roll();
+        event.preventDefault();
+    }
+}
+
+var gameconsole = new GameConsole();
+$(document).ready(function () {
+    console.log('ready!');
+    $(document).on('keydown', ehandler);
+    $('#grid').on('tap', ehandler);
+});
+
+//}
